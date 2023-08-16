b0104938304e3b11e8fc1f3db73537784fd6bcc4
Luiz Henrique Pereira <luuiz.pereira.correa@gmail.com>
2023-08-16 20:36:44 -0300

    correção de lista de horarios

--- src/br/com/senai/core/dao/postgres/DaoPostgresRestaurante.java
+++ src/br/com/senai/core/dao/postgres/DaoPostgresRestaurante.java
@@ -10,6 +10,7 @@ import br.com.senai.core.dao.DaoRestaurante;
 import br.com.senai.core.dao.ManagerDb;
 import br.com.senai.core.domain.Categoria;
 import br.com.senai.core.domain.Endereco;
+import br.com.senai.core.domain.Horario;
 import br.com.senai.core.domain.Restaurante;
 
 public class DaoPostgresRestaurante implements DaoRestaurante {
@@ -43,6 +44,12 @@ public class DaoPostgresRestaurante implements DaoRestaurante {
 	private final String COUNT_BY_CATEG = "SELECT Count(*) qtde "
 										 + "FROM restaurantes r "
 										 + "WHERE r.id_categoria = ?";
+	
+	private final String SELECT_TODOS ="SELECT r.id id_restaurante , r.nome nome_restaurante , r.descricao , r.cidade ,r.logradouro, "
+			  +"r.bairro , r.complemento ,c.id id_categoria , c.nome nome_categoria "
+			  +"FROM restaurantes r ,categorias c "
+			  +"WHERE r.id_categoria = c.id ORDER BY r.nome" ; 
+	
 	
 	private Connection conexao;
 	
@@ -276,9 +283,33 @@ public class DaoPostgresRestaurante implements DaoRestaurante {
 	}
 
 	@Override
-	public List<Restaurante> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+    public List<Restaurante> listarTodos() {
+		
+		List<Restaurante> restaurantes = new ArrayList<Restaurante>();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			ps = conexao.prepareStatement(SELECT_TODOS);
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				restaurantes.add(extrairDo(rs));
+			}
+				
+			return restaurantes ;
+			
+		} catch (Exception e) {
+			
+			throw new RuntimeException("ocorreu um erro ao buscar todos os restaurante " + e.getMessage()) ;
+	
+		}finally {
+			
+			ManagerDb.getInstance().fechar(ps);
+			ManagerDb.getInstance().fechar(rs);
+			
+		}
+		
 	}
 
 }
